884e64a8befbc641360272bd94ad30b47868e1fa
Mahima Meena <your-email@gmail.com>
2026-01-16 22:56:12 +0530

    Add changes

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
 let usersInputSection = document.getElementById("usersInputSection");
 let categoryInputSection = document.getElementById("categoryInputSection");
 let displayQuestionSection = document.getElementById("displayQuestionSection");
+let roundSummary = document.getElementById("roundSummary");
 let displayWinnersSection = document.getElementById("displayWinnersSection");
 
-let player1Input = document.getElementById("player1");
-let player2Input = document.getElementById("player2");
-
-let categories = document.getElementById("categories");
-let player1NameDiv = document.getElementById("player1NameDiv");
-let player2NameDiv = document.getElementById("player2NameDiv");
-
-let questionSection = document.getElementById("questionSection");
-let currentPlayerScore = document.getElementById("currentPlayerScore");
-let nextQuestionBtn = document.getElementById("nextQuestionBtn");
 let radio_options = document.getElementsByClassName("radio_options");
-let optionsDiv = document.getElementById("optionsDiv");
+let questionSection=document.getElementById("questionSection");
+let optionsDiv=document.getElementById("optionsDiv");
 let error2 = document.getElementById("error2");
-
-let nextRoundBtn = document.getElementById("nextRoundBtn");
-let endGameBtn = document.getElementById("endGameBtn");
-
+let nextQuestionBtn = document.getElementById("nextQuestionBtn");
 
 categoryInputSection.style.display = "none";
 displayQuestionSection.style.display = "none";
+roundSummary.style.display = "none";
 displayWinnersSection.style.display = "none";
-nextQuestionBtn.style.display = "none";
-nextRoundBtn.style.display = "none";
-endGameBtn.style.display = "none";
+nextQuestionBtn.disabled = true;
 
 let selectedCategories = [];
+let selectedCategory="";
 let allQuestions = [];
-let category = "";
 let currentTurn = "";
 let index = 0;
 let questionCount = 1;
 let round = 1;
 let player1_score = 0;
 let player2_score = 0;
+let player1Name = "";
+let player2Name = "";
 
 const allCategories = [
   "music",
@@ -53,7 +43,12 @@ const allCategories = [
 
 let startGameBtn = document.getElementById("startGameBtn");
 startGameBtn.addEventListener("click", function () {
+  let player1Input = document.getElementById("player1");
+  let player2Input = document.getElementById("player2");
+  let player1Value = document.getElementById("player1Value");
+  let player2Value = document.getElementById("player2Value");
   let error = document.getElementById("error");
+
   if (player1Input.value.trim() === "" || player2Input.value.trim() === "") {
     error.innerText = "Please Enter Both Players Name";
     return;
@@ -66,51 +61,57 @@ startGameBtn.addEventListener("click", function () {
     usersInputSection.style.display = "none";
     categoryInputSection.style.display = "block";
 
-    player1NameDiv.innerText = player1Input.value;
-    player2NameDiv.innerText = player2Input.value;
+    player1Name = player1Input.value;
+    player2Name = player2Input.value;
+
+    player1Value.innerText = player1Name;
+    player2Value.innerText = player2Name;
   }
 });
 
 let startRoundBtn = document.getElementById("startRoundBtn");
 startRoundBtn.addEventListener("click", function () {
+  let categories = document.getElementById("categories");
   let error1 = document.getElementById("error1");
   if (categories.value === "default") {
     error1.innerText = "Please select any category";
     return;
   } else {
     error1.innerText = "";
-    category = categories.value;
-    selectedCategories.push(category);
-
-    currentTurn = player1Input.value;
+    selectedCategory = categories.value;
+    selectedCategories.push(selectedCategory);
+    currentTurn = player1Name;
     categoryInputSection.style.display = "none";
     displayQuestionSection.style.display = "block";
-    optionsDiv.style.display = "none";
-    questionSection.style.display = "none";
-     error2.innerText = "Questions Fetching....."
+     error2.innerText = "Loading....."
     fetchingQuestions();
+     optionsDiv.style.display="none";
+     questionSection.style.display="none";
   }
 });
 
 async function fetchingQuestions() {
   try {
     const response1 = await fetch(
-      `https://the-trivia-api.com/v2/questions?categories=${category}&difficulties=easy&limit=2`
+      `https://the-trivia-api.com/v2/questions?categories=${selectedCategory}&difficulties=easy&limit=2`
     );
     let easyQuestions = await response1.json();
 
     const response2 = await fetch(
-      `https://the-trivia-api.com/v2/questions?categories=${category}&difficulties=medium&limit=2`
+      `https://the-trivia-api.com/v2/questions?categories=${selectedCategory}&difficulties=medium&limit=2`
     );
     let mediumQuestions = await response2.json();
 
     const response3 = await fetch(
-      `https://the-trivia-api.com/v2/questions?categories=${category}&difficulties=hard&limit=2`
+      `https://the-trivia-api.com/v2/questions?categories=${selectedCategory}&difficulties=hard&limit=2`
     );
     let hardQuestions = await response3.json();
 
     allQuestions = [...easyQuestions, ...mediumQuestions, ...hardQuestions];
     showQuestion();
+     optionsDiv.style.display="block";
+     questionSection.style.display="block";
+     error2.innerText="";
   } catch (err) {
     console.log("error fetching questions");
   }
@@ -142,17 +143,12 @@ function showQuestion() {
   document.getElementById("third_option_text").innerText = options[2];
   document.getElementById("fourth_option_text").innerText = options[3];
 
-  optionsDiv.style.display = "block";
-  nextQuestionBtn.style.display = "none";
-  questionSection.style.display = "block";
-  error2.innerText = "";
-
   roundNumber.innerText = `Round ${round}`;
   questionNumber.innerText = `Question Number:-  ${questionCount}`;
   questionCategory.innerText = `Question Category:- ${question.category} `;
   questionDifficulty.innerText = `Question Difficulty:- ${question.difficulty}`;
   currentPlayerTurn.innerText = `Current Player's Turn:- ${currentTurn}`;
-  playersScore.innerText = ` ${player1Input.value}:- ${player1_score}  ${player2Input.value}:- ${player2_score}`;
+  playersScore.innerText = ` ${player1Name}:- ${player1_score}  ${player2Name}:- ${player2_score}`;
   questionText.innerText = `Question:- ${question.question.text}`;
 
   for (let i = 0; i < radio_options.length; i++) {
@@ -171,6 +167,8 @@ for (i = 0; i < radio_options.length; i++) {
 }
 
 function showAnswer(selectedOption) {
+  let error2 = document.getElementById("error2");
+  let currentPlayerScore = document.getElementById("currentPlayerScore");
   let currentQuestion = allQuestions[index];
   let score = 0;
   if (selectedOption === currentQuestion.correctAnswer) {
@@ -182,30 +180,32 @@ function showAnswer(selectedOption) {
     } else {
       score = 20;
     }
-    if (currentTurn === player1Input.value) {
+    if (currentTurn === player1Name) {
       player1_score += score;
       let current_score1 = score;
-      currentPlayerScore.innerText = `${player1Input.value}:- Got ${current_score1} `;
+      currentPlayerScore.innerText = `${player1Name}:- Got ${current_score1} `;
     } else {
       player2_score += score;
       let current_score2 = score;
-      currentPlayerScore.innerText = `${player2Input.value}:- Got ${current_score2}`;
+      currentPlayerScore.innerText = `${player2Name}:- Got ${current_score2}`;
     }
   } else {
     error2.innerText = `❌ Wrong Answer! Correct answer is: ${currentQuestion.correctAnswer}`;
   }
-  nextQuestionBtn.style.display = "block";
+
+  nextQuestionBtn.disabled = false;
 }
 
 nextQuestionBtn.addEventListener("click", function () {
+  nextQuestionBtn.disabled = true;
   currentPlayerScore.innerText = "";
   error2.innerText = "";
   index++;
   questionCount++;
-  if (currentTurn === player1Input.value) {
-    currentTurn = player2Input.value;
+  if (currentTurn === player1Name) {
+    currentTurn = player2Name;
   } else {
-    currentTurn = player1Input.value;
+    currentTurn = player1Name;
   }
   if (index < allQuestions.length) {
     showQuestion();
@@ -213,21 +213,19 @@ nextQuestionBtn.addEventListener("click", function () {
     if (selectedCategories.length === allCategories.length) {
       nextRoundBtn.disabled = true;
     }
-    nextQuestionBtn.style.display = "none";
-    nextRoundBtn.style.display = "block";
-    endGameBtn.style.display = "block";
+    roundSummary.style.display = "block";
+    displayQuestionSection.style.display = "none";
   }
 });
 
+let nextRoundBtn = document.getElementById("nextRoundBtn");
 nextRoundBtn.addEventListener("click", function () {
   let countRound = document.getElementById("countRound");
   round++;
   countRound.innerHTML = `Round:- ${round}`;
   index = 0;
   questionCount = 1;
-  nextRoundBtn.style.display = "none";
-  displayQuestionSection.style.display = "none";
-  endGameBtn.style.display = "none";
+  roundSummary.style.display = "none";
   categoryInputSection.style.display = "block";
 
   const leftCategories = allCategories.filter(function (category) {
@@ -243,20 +241,21 @@ nextRoundBtn.addEventListener("click", function () {
   });
 });
 
+let endGameBtn = document.getElementById("endGameBtn");
 endGameBtn.addEventListener("click", function () {
   let result = document.getElementById("result");
-  displayQuestionSection.style.display = "none";
+  roundSummary.style.display = "none";
   displayWinnersSection.style.display = "block";
 
   if (player1_score > player2_score) {
-    result.innerText = `${player1Input.value} Won `;
+    result.innerText = `${player1Name} Won `;
   } else if (player1_score < player2_score) {
-    result.innerText = `${player2Input.value} Won `;
+    result.innerText = `${player2Name} Won `;
   } else {
     result.innerText = "It's a Draw!";
   }
   let player1Status = document.getElementById("player1Status");
   let player2Status = document.getElementById("player2Status");
-  player1Status.innerText = ` ${player1Input.value} Score is :- ${player1_score}`;
-  player2Status.innerText = ` ${player2Input.value} Score is :- ${player2_score}`;
+  player1Status.innerText = ` ${player1Name} Score is :- ${player1_score}`;
+  player2Status.innerText = ` ${player2Name} Score is :- ${player2_score}`;
 });
